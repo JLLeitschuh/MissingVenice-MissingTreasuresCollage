1c08fd006ba67e86855da2afe19f949e6767e1ca
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2014-11-04 13:39:37 +0100

    Adds more functionality to the StandardizedDataSet

--- javascripts/dataLoad.js
+++ javascripts/dataLoad.js
@@ -1,20 +1,41 @@
 function StandardizedDataSet(object, id, parentDataName){
 	this.parentDataName = parentDataName;
 	this.id = id;
+	function ImageURLData(imageDataSet, width, height){
+		this.thumb = imageDataSet.thumb;
+		this.small = imageDataSet.small;
+		this.medium = imageDataSet.medium;
+		this.original = imageDataSet.original;
+		this.width = width;
+		this.height = height;
+	}
+	
+	/*
+	 * All of this data has been custom taylored to the existing datasets. The best way to do this is to print the object to the console
+	 * and manually find the fields that work with this object.
+	 */
 	switch(parentDataName){
 		case 'Demolished Churches merge':
 			//This is the id that the image url is stored behind
 			var mediaID = object['merged-media-ids'].images['Demolished Churches Media'];
 			this.name = object.data.name;
 			this.shortDescription = object.data.description;
-			this.imageSmallUrl = object.media.images[mediaID].small;
-			this.imageWidth = object.data.width;
-			this.imageHeight = object.data.height;
+			this.sections = {'Header 1':'A lot of text'};
+			this.imageData = [new ImageURLData(object.media.images[mediaID], object.data.width, object.data.height)];
 			break;
 		case 'Venice Churches':
-			this.name = theObject.data["Page Title"];
+			//This is the id that the image url is stored behind
+			var mediaID = object['merged-media-ids'].images['Church Facade Images 2012'];
+			this.name = object.data["Page Title"];
 			//Make this more descriptive. There is more data here.
-			this.shortDescription = theObject.data['History Blurb'];
+			this.shortDescription = object.data['History Blurb'];
+			this.imageData = [new ImageURLData(object.media.images[mediaID], object.data.width, object.data.height)];
+			break;
+		case 'Convents Merge':
+			var mediaID = object['merged-media-ids'].images['convents facade images'];
+			this.name = object.data['Full Name'];
+			this.shortDescription = "Current Use: " + object.data['Current Use'];
+			this.imageData = [new ImageURLData(object.media.images[mediaID], object.data.width, object.data.height)];
 			break;
 		default:
 			this.name = "Unsuported Data Set";
@@ -26,9 +47,9 @@ function StandardizedDataSet(object, id, parentDataName){
 		return new ImageWrapper(this.name,
 			this.year,
 			this.shortDescription,
-			this.imageSmallUrl,
-			this.imageWidth,
-			this.imageHeight,
+			this.imageData[0].small,
+			this.imageData[0].width,
+			this.imageData[0].height,
 			this.parentDataName,
 			this.id
 		);
@@ -52,8 +73,11 @@ function loadImageData(imageArray, tagsArray){
 		imgLoad.on( 'always', onAlways );
 	}
 	
+	/*******************************************
+	 ALL CK Console Requests Defined
+	*******************************************/
 	/**
-	 * 
+	 * Collects the data for a single dataset.
 	 */
 	function DataSetCollector(dataName, categories, inputParser){
 		this.dataName = dataName;
@@ -91,16 +115,13 @@ function loadImageData(imageArray, tagsArray){
 			var church = input.members[property];
 			if(church.data["Current Use"] != "Active Church"){
 				try{
-					var mediaID = church['merged-media-ids'].images['Church Facade Images 2012'];
-					imageArray[imageArray.length] = new ImageWrapper(church.data['Page Title'],
-						church.data['Year Founded'],
-						church.data['Intro sentence'],
-						church.media.images[mediaID].small,
-						church.data.width,
-						church.data.height,
-						this.dataName,
-						property
-					);
+					//This is the individual peice of data pulled form the list
+					var church = input.members[property];
+					//console.log(church);
+					
+					var dataSet = new StandardizedDataSet(church, property, this.dataName);
+					//Create and add a new image to the image array using the church data
+					imageArray[imageArray.length] = dataSet.getImageWrapper();
 				} catch (e){
 					console.log("No media associated");
 				}
@@ -110,24 +131,25 @@ function loadImageData(imageArray, tagsArray){
 	
 	var conventsCollector = new DataSetCollector('Convents Merge', ['Convents'], function (input){
 		for (var property in input.members) {
+			//This is the individual peice of data pulled form the list
 			var convent = input.members[property];
-			var mediaID = convent['merged-media-ids'].images['convents facade images'];
-			imageArray[imageArray.length] = new ImageWrapper(convent.data['Full Name'],
-						convent.data['Year Founded'],
-						"Current Use: " + convent.data['Current Use'],
-						convent.media.images[mediaID].small,
-						convent.data.width,
-						convent.data.height,
-						this.dataName,
-						property
-						);
+			try{
+				var dataSet = new StandardizedDataSet(convent, property, this.dataName);
+				//Create and add a new image to the image array using the church data
+				imageArray[imageArray.length] = dataSet.getImageWrapper();
+			} catch (e){
+				console.log("No media associated");
+			}
 		}
 	});
 	
 	app = angular.module('collageapp', ['ckServices']);
 	app.controller('DataCtrl', ['$scope', '$compile', 'ckConsole', 'ckConsoleMap', '$http', function($scope, $compile, ckConsole, ckConsoleMap, $http){
 		
-		
+		/**
+		 * Multi tiered function. In order to use you pass the function a dataSetCollector and the next funtion in the change to run.
+		 * The function will make a request for the given data set if it is valid for the current search tags.
+		 */
 		function getDataset(aDataSetCollector, nextFunction){
 			if(aDataSetCollector.doAnyArgumentsMatch(tagsArray)){
 				ckConsole.getGroup(aDataSetCollector.dataName).then(function(inputData){
@@ -149,18 +171,21 @@ function loadImageData(imageArray, tagsArray){
 		
 		getDataset(demolishedChurchesCollector, function(){
 			getDataset(veniceChurchesCollector, function(){
-				//getDataset(conventsCollector, attachImagesToScope);
-				attachImagesToScope();
+				getDataset(conventsCollector, attachImagesToScope);
+				//attachImagesToScope();
 			});
 		});
 		
 	}]);
 }
 
+/**
+ * Loads the object data for a single data set given a given dataset name and id.
+ */
 function loadObjectData(dataSetName, id){
 	app = angular.module('collageapp', ['ckServices']);
 	app.controller('DataCtrl', ['$scope', '$compile', 'ckConsole', 'ckConsoleMap', '$http', function($scope, $compile, ckConsole, ckConsoleMap, $http){
-		if(dataSetName == "" || dataSetName == "Unsuported"){
+		if((dataSetName == "") || (dataSetName == "Unsuported")){
 			$scope.data.name = "No data set requested";
 			$scope.data.shortDescription = "You must request a specific dataset";
 		} else {
@@ -168,7 +193,9 @@ function loadObjectData(dataSetName, id){
 				var theObject = inputData.members[id];
 				var collector;
 				console.log(theObject);
+				console.log("StandardizedDataSet");
 				$scope.data = new StandardizedDataSet(theObject, id, dataSetName);
+				console.log($scope.data);
 				
 			});
 		}
