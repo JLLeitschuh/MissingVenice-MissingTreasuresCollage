8409f319deb13d594944265f5ed6943eb22ca4ac
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2014-11-06 15:41:07 +0100

    Adds an image search function to the collage (Not permanent location)

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -3,6 +3,24 @@ angular.module('ArtifactFeederApp.controllers', []).
 		console.log("ArtifactFeederApp.controllers: artifactsController");
 		$scope.nameFilter = null;
 
+		var resizeTimer = null;
+		var collageImagesFunction = function() {
+			// hide all the images until we resize them
+			$('.Collage .Image_Wrapper').css("opacity", 0);
+			// set a timer to re-apply the plugin
+			if (resizeTimer) clearTimeout(resizeTimer);
+			resizeTimer = setTimeout(function(){
+				// We have to remove the caption manually or it will be left over on some images.
+				$("div.Caption").remove();
+				collage();
+				$('.Collage').collageCaption();
+			}, 250);
+		};
+
+		$scope.searchChange = collageImagesFunction;
+
+		// This is just for the case that the browser window is resized
+		$(window).bind('resize', collageImagesFunction);
 	}).
 	controller('artifactController', function($scope, ArtifactService){
 		console.log("ArtifactFeederApp.controllers: artifactController");
