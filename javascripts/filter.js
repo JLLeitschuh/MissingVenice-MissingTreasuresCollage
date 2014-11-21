83159e69a9c0891efea3a5dd6e098460e33c4474
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2014-11-21 12:00:26 +0100

    Adds a filter that removes undefined values.

--- javascripts/filter.js
+++ javascripts/filter.js
@@ -1,56 +1,89 @@
-angular.module('ArtifactFeederApp.filters', [])
+/**
+ * Extracts the value from the given item using the
+ * given filterOn value.
+ */
+var extractValueToCompare = function (item, filterOn) {
+	if (angular.isObject(item) && angular.isString(filterOn)) {
 
-	.filter('unique', function () {
-	return function (items, filterOn) {
-
-		if (filterOn === false) {
-			return items;
+		/**
+		 * Recursive function that resolves the filterOn
+		 * returns the object that is resolved from the search
+		 *         or undefined if the string location does not exist
+		 */
+		var resolveSearch = function(object, string){
+			if(typeof object == 'undefined'){
+				return object;
+			}
+			var values = string.split(".");
+			var firstValue = values[0];
+			string = string.replace(firstValue + ".", "");
+			if(values.length > 1){
+				return resolveSearch(object[firstValue], string);
+			} else {
+				return object[firstValue];
+			}
 		}
 
-		if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
-			var hashCheck = {}, newItems = [];
+		return resolveSearch(item, filterOn);
+	} else {
+		return item;
+	}
+};
 
-			var extractValueToCompare = function (item) {
-				if (angular.isObject(item) && angular.isString(filterOn)) {
 
-					var resolveSearch = function(object, string){
-						if(typeof object == 'undefined'){
-							return object;
+angular.module('ArtifactFeederApp.filters', [])
+	/*****************************
+	 Filter removes duplicates.
+	*****************************/
+	.filter('unique', function () {
+		return function (items, filterOn) {
+
+			if (filterOn === false) {
+				return items;
+			}
+
+			if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+				var hashCheck = {}, newItems = [];
+
+				angular.forEach(items, function (item) {
+					var valueToCheck, isDuplicate = false;
+
+					for (var i = 0; i < newItems.length; i++) {
+						if (angular.equals(extractValueToCompare(newItems[i], filterOn),
+						                   extractValueToCompare(item, filterOn))) {
+							isDuplicate = true;
+							break;
 						}
-						var values = string.split(".");
-						var firstValue = values[0];
-						string = string.replace(firstValue + ".", "");
-						if(values.length > 1){
-							return resolveSearch(object[firstValue], string);
-						} else {
-							return object[firstValue];
+					}
+					if (!isDuplicate) {
+						if(typeof item != 'undefined'){
+							newItems.push(item);
 						}
 					}
 
-					return resolveSearch(item, filterOn);
-				} else {
-					return item;
-				}
-			};
-
-			angular.forEach(items, function (item) {
-				var valueToCheck, isDuplicate = false;
-
-				for (var i = 0; i < newItems.length; i++) {
-					if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
-						isDuplicate = true;
-						break;
-					}
-				}
-				if (!isDuplicate) {
-					if(typeof item != 'undefined'){
+				});
+				items = newItems;
+			}
+			return items;
+		};
+	})
+	/*****************************
+	 Filter removes blank and
+	 undefined values.
+	*****************************/
+	.filter('noBlank', function(){
+		return function(items, filterOn){
+			//console.log("noBlank");
+			var newItems = [];
+			if(angular.isArray(items)){
+				angular.forEach(items, function(item){
+					var extracted = extractValueToCompare(item, filterOn);
+					if(angular.isDefined(extracted) && !angular.equals(extracted, "")){
 						newItems.push(item);
 					}
-				}
-
-			});
-			items = newItems;
-		}
-		return items;
-	};
-});
+				});
+				items = newItems;
+			}
+			return items;
+		};
+	});
